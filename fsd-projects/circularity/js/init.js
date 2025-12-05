23c49b6b0267645806b6babd1a4e16633f87cdc8
Jxsuemtz100 <jf017682@gmail.com>
2025-12-05 13:38:17 +0000

    finish

--- fsd-projects/circularity/js/init.js
+++ fsd-projects/circularity/js/init.js
@@ -25,7 +25,7 @@ var init = function (window) {
 
         // TODO 2 : Create a function that draws a circle 
         function drawCircle(){
-        circle = draw.randomCircleInArea(canvas, true, true, "#999", 2);
+        circle = draw.randomCircleInArea(canvas, true, true, "#296372ff", 2);
          physikz.addRandomVelocity(circle, canvas, 5, 5);
          view.addChild(circle);
         circles.push(circle);
@@ -40,7 +40,7 @@ var init = function (window) {
        // drawCircle([3]);
        // drawCircle([4]);
         // TODO 7 : Use a loop to create multiple circles
-        for (var i = 0; i < 100; i++) {
+        for (var i = 0; i <= 100; i++) {
             drawCircle();
         }
 
--- fsd-projects/platformer/platformer.js
+++ fsd-projects/platformer/platformer.js
@@ -31,46 +31,87 @@ $(function () {
 
 
     // TODO 2 - Create Platforms
-    createPlatform(1000, 300, 210, 20);
-    createPlatform(1200, 420, 200, 10, "red");
-    createPlatform(300, 700, 90, 20, "orange");
-    createPlatform(700, 570, 70, 20, "purple");
-    createPlatform(1000, 500, 100, 20, "blue");
-    createPlatform(525, 570, 10, 5, "black");
-    createPlatform(1000, 0, 20, 300);
-    createPlatform(820,480, 70, 10 );
-    createPlatform(1000, 0, 500, 10);
-    createPlatform(1150, 175, 10, 10);
-    createPlatform(1330, 135, 10, 90);
-    createPlatform(1300, 220, 100, 10);
-    createPlatform(600,400,70,10, "lime");
-    createPlatform(400, 400, 100, 10);
-    createPlatform(500, 200, 10, 70);
-    createPlatform(400,200,10,100, "blue");
-   createPlatform(400,300,10,100);
-  createPlatform(400,400,120, 10);
-   createPlatform(400,100,10,100);
-    createPlatform(500, 100, 10,170);
-    createPlatform(500,0,10,170);
-    createPlatform(300,100,100,10);
-    createPlatform(400,300,30,5);
-    createPlatform(480, 200, 30,5);
-    createPlatform(300,100,10, 310);
-    createPlatform(200, 400, 100,10);
-    createPlatform(200, 0, 10, 400);
-    createPlatform(200, 0, 300,10);
-    createPlatform(280, 200, 20, 5);
-    createPlatform(200,300, 30, 5);
+  //   createPlatform(1000, 300, 210, 20);
+  //   createPlatform(1200, 420, 200, 10, "red");
+  
+    
+   
+  
+  //   createPlatform(1000, 0, 20, 300);
+  //   createPlatform(820,480, 70, 10 );
+  //   createPlatform(1000, 0, 500, 10);
+  //   createPlatform(1150, 175, 10, 10);
+  //   createPlatform(1330, 135, 10, 90);
+  //   createPlatform(1300, 220, 100, 10);
+    
+  //   createPlatform(400, 400, 100, 10);
+  //   createPlatform(500, 200, 10, 70);
+  //   createPlatform(400,200,10,100,);
+  //  createPlatform(400,300,10,100);
+  // createPlatform(400,400,120, 10);
+  //  createPlatform(400,100,10,100);
+  //   createPlatform(500, 100, 10,170);
+  //   createPlatform(500,0,10,170);
+  //   createPlatform(300,100,100,10);
+  //   createPlatform(400,300,30,5);
+  //   createPlatform(480, 200, 30,5);
+  //   createPlatform(300,100,10, 310);
+  //   createPlatform(200, 400, 100,10);
+  //   createPlatform(200, 0, 10, 400);
+  //   createPlatform(200, 0, 300,10);
+  //   createPlatform(280, 200, 20, 5);
+  //   createPlatform(200,300, 30, 5);
+//12/1/25 New platformer classwork
+createPlatform(200, 640, 20, 100, "transparent");
+createPlatform(300, 680, 100, 20, "orange");
+createPlatform(200, 450, 20, 100, "transparent");
+createPlatform(415, 610, 100, 20, "orange");
+createPlatform(600, 400, 10, 120, 'transparent');
+createPlatform(590, 480, 10, 5, "transparent");
+createPlatform(1000, 300, 210, 20);
+createPlatform(1200, 420, 200, 10, "orange");
+createPlatform(1000, 0, 20, 300,);
+createPlatform(820,480, 70, 10 );
+createPlatform(1000, 0, 500, 10,);
+createPlatform(1150, 175, 10, 10, );
+createPlatform(1330, 135, 10, 90);
+createPlatform(1300, 220, 100, 10);
+createPlatform(400, 400, 100, 10);
+createPlatform(500, 200, 10, 70);
+createPlatform(400,200,10,100,);
+createPlatform(400,300,10,100);
+createPlatform(400,400,120, 10);
+createPlatform(400,100,10,100);
+createPlatform(500, 100, 10,170);
+createPlatform(500,0,10,170);
+createPlatform(300,100,100,10);
+createPlatform(400,300,30,5);
+createPlatform(480, 200, 30,5);
+createPlatform(300,100,10, 310);
+createPlatform(200, 400, 100,10);
+createPlatform(200, 0, 10, 400);
+createPlatform(200, 0, 300,10);
+createPlatform(280, 200, 20, 5);
+createPlatform(200,300, 30, 5);
+createPlatform(1000, 500, 200, 14, 'orange')
+
+
+
     // TODO 3 - Create Collectables
-   createCollectable("steve", 510, 490, 1.5, 1.0);
+   createCollectable("grace", 570, 470, 1.5, 1.0);
    createCollectable("diamond", 1340, 100, 1.5, 1.0);
    createCollectable("max", 330, 50, 1.5, 1.0);
    createCollectable("steve", 230, 300, 1.5, 1.0);
+
+
     // TODO 4 - Create Cannons
-      createCannon("bottom", 1300, 1200);
-      createCannon("bottom", 530, 800);
+      createCannon("bottom", 1300, 1490);
+      createCannon("bottom", 530, 1600);
       createCannon("bottom", 390, 3600);
       createCannon("bottom", 200, 2900, );    
+
+
+
     //////////////////////////////////
     // ONLY CHANGE ABOVE THIS POINT //
     //////////////////////////////////
